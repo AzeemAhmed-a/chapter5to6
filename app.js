023215ae3e3970c033143925fc3760a32319a7ab
AzeemAhmed-a <154541735+AzeemAhmed-a@users.noreply.github.com>
2024-02-29 12:59:45 +0500

    chapter 5 to 7

---
+++ app.js
@@ -0,0 +1,109 @@
+
+//Q..1..Given var x = 25 and var y = 7, what is the result of x % y?
+var x = 25 
+var y = 7
+
+var modulas =25 % 7
+console.log (modulas)
+
+
+//Q..2..If var a = 15 and var b = 4, what is the value of a * b after adding 10 to a and subtracting 2 from b?
+var A = 15 
+var B = 4
+
+var bodmas =15 * 4 +10 -2
+console.log (bodmas)
+
+//Q..3..3. Suppose var c = 12 and var d = 3, calculate the result of c ** d.
+var c = 12 
+var d = 3
+
+var multiplication =12 ** 3
+console.log (multiplication)
+
+
+//Q..4 For var e = 10 and var f = 3, what is the value of e += f * 2?
+var e = 10 
+var f = 3
+
+var bodmas =10 + 3  * 2
+console.log (bodmas)
+
+
+//Q.5..Considering var g = 20 and var h = 4, what will be the outcome of g / h after adding 5 to g?
+var g = 20 
+var h = 4
+
+var adding =20 + 4  + 5
+console.log (adding)
+
+
+//Q.6..Given var i = 8 and var j = 3, determine the value of i * j after subtracting 2 from i.?
+var i = 8 
+var j = 3
+
+var subtracting =8 + 3  - 2
+console.log (subtracting)
+
+//Q.07..If var k = 18 and var l = 5, what is the result of k - l + 2??
+var k = 18 
+var l = 5
+
+var bodmas  =18 - 5  + 2
+console.log (bodmas )
+
+//Q.08..Suppose var m = 7 and var n = 2, calculate the value of m ** n.?
+var m = 7 
+var n = 2
+
+var multiplication =7 ** 2  
+console.log (multiplication)
+
+//Q.09..For var o = 13 and var p = 4, what will be the outcome of o % (p + 2)??
+var o = 13 
+var p = 4
+
+var modulas =13 % 4  +2
+console.log (modulas)
+
+//Q.10..10. Given var q = 16 and var r = 3, determine the result of q * r after dividing q by 2.?
+var q = 16 
+var r = 3
+
+var bodmas =16 / 2  * 3
+console.log (bodmas)
+
+//Q.11..If var s = 25 and var t = 3, what is the value of s / t after subtracting 1 from s??
+var s = 25 
+var t = 3
+
+var bodmas =25 - 1  / 3
+console.log (bodmas)
+
+//Q.12..Suppose var u = 11 and var v = 3, calculate the value of u - v after adding 2 to v.?
+var u = 11
+var v = 3
+
+var bodmas =11 / 2  - 3
+console.log (bodmas)
+
+//Q.13..For var w = 22 and var x = 5, what will be the outcome of w + (x ** 2)??
+var w = 22 
+var x = 5
+
+var bodmas =22 +  5 ** 2
+console.log (bodmas)
+
+//Q.14..Given var y = 19 and var z = 4, determine the result of y % (z + 1).?
+var y = 19 
+var z = 4
+
+var bodmas =19 % 4  + 1
+console.log (bodmas)
+
+//Q.15..If var a = 10 and var b = 3, what is the value of a * (b - 2)??
+var a = 10 
+var b = 3
+
+var bodmas =10 * 3  - 2
+console.log (bodmas)
